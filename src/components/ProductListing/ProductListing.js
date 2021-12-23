0cec9dec618f8b9ef900b7fd6cd8cc023edd133f
RRR-09 <79768142+RRR-09@users.noreply.github.com>
2021-12-22 22:13:04 -0500

    Build fix

---
+++ src/components/ProductListing/ProductListing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// import { breakpoints, spacing } from '../../utils/styles';
+// import ProductListingHeader from './ProductListingHeader';
+// import ProductListingItem from './ProductListingItem';
+
+
+
+// const ProductListingContainer = styled(`div`)`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   padding: ${spacing.lg}px;
+//   @media (min-width: ${breakpoints.desktop}px) {
+//     flex-direction: row;
+//     flex-wrap: wrap;
+//     padding: ${spacing['2xl']}px;
+//   }
+// `;
+
+const ProductListing = () => (
+    <>
+        <p>Test</p>
+    </>
+);
+
+export default ProductListing;
--- src/pages/store/index.js
+++ src/pages/store/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ProductListing from '../components/ProductListing';
+import ProductListing from '../../components/ProductListing';
 
 const IndexPage = () => <ProductListing />;
 
