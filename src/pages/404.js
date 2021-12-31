d816d26661f5b4c4862390fb1e2a06498b57a407
RRR-09 <79768142+RRR-09@users.noreply.github.com>
2021-12-31 03:43:22 -0500

    Added better 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,23 @@
-import * as React from "react"
-import { Link } from "gatsby"
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
+import React from "react";
+import BackgroundVideo from "../components/BackgroundVideo";
+import FooterJoinButton from "../components/FooterJoinButton";
+import Navbar from "../components/Navbar";
+import { BackgroundCard } from "../components/Store/styles-grid";
+import "../styles/styles.css";
+const NotFoundPage = ({ location }) => {
   return (
-    <main style={pageStyles}>
+    <main>
       <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+      <BackgroundVideo />
+      <div id="wrapper">
+        <BackgroundCard style={{ height: "50vh", marginTop: "5vh" }}>
+          <h1>Sorry, we couldn't find what you were looking for!</h1>
+        </BackgroundCard>
+      </div>
 
-export default NotFoundPage
+      <Navbar currentPage={location.pathname} />
+      <FooterJoinButton />
+    </main>
+  );
+};
+export default NotFoundPage;
