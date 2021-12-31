d3c2b00339bab0debbf54b9491df3ef0d67cf8a9
RRR-09 <79768142+RRR-09@users.noreply.github.com>
2021-12-31 00:29:02 -0500

    Fix monthly goal display

--- src/components/MonthlyGoal/MonthlyGoal.js
+++ src/components/MonthlyGoal/MonthlyGoal.js
@@ -1,35 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
-export const spacing = {
-  "3xs": 2,
-  "2xs": 4,
-  xs: 8,
-  sm: 12,
-  md: 16,
-  lg: 24,
-  xl: 32,
-  "2xl": 40,
-  "3xl": 48,
+const MonthlyGoal = () => {
+  const [progress, setProgress] = useState(0);
+
+  const Row = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: stretch;
+    width: 100%;
+    padding-top: 1.5em;
+  `;
+  const ProgressLabel = styled.label`
+    display: block;
+    text-shadow: 2px 2px 4px #000000;
+    color: #eee;
+    font-family: "Lato", sans-serif;
+    font-weight: bold;
+    display: block;
+    font-size: 21px;
+    color: #fff;
+    font-size: 21px;
+    margin-bottom: 6px;
+    text-align: center !important;
+  `;
+  const ProgressContainer = styled.div`
+    color: #000 !important;
+    background-color: #f1f1f1 !important;
+    margin-bottom: 12px;
+    width: 100%;
+  `;
+  const Progress = styled.div`
+    width: ${progress}%;
+    transition: width 2s;
+    color: ${progress > 1 ? "#fff" : "#000"};
+    background-color: #4caf50 !important;
+    text-align: center !important;
+    height: 24px;
+  `;
+
+  return (
+    <Row>
+      <ProgressLabel>Monthly Goal Progress:</ProgressLabel>
+      <ProgressContainer>
+        <Progress>0%</Progress>
+      </ProgressContainer>
+    </Row>
+  );
 };
-
-export const breakpoints = {
-  mobile: 400,
-  phablet: 550,
-  tablet: 750,
-  desktop: 1000,
-  hd: 1300,
-};
-
-const MonthlyGoal = () => (
-  <div className="row">
-    <div className="col-sm-12">
-      <label id="monthlyGoalProgressLabel">Monthly Goal Progress:</label>
-      <div id="monthlyGoalProgressContainer" align="left">
-        <div id="monthlyGoalProgress" style={{ width: "0%" }}>
-          0%
-        </div>
-      </div>
-    </div>
-  </div>
-);
 export default MonthlyGoal;
